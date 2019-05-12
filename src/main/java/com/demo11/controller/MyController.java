8aff37c24b27ce67146579cc0d8d12fee6a1c9ad
admin <sem00000@163.com>
2019-05-12 23:56:28 +0800

    test2.....

--- src/main/java/com/demo11/controller/MyController.java
+++ src/main/java/com/demo11/controller/MyController.java
@@ -10,4 +10,9 @@ public class MyController {
     public String test1(){
         return "123456789";
     }
+
+    @RequestMapping("/test2")
+    public String test2(){
+        return "abcdefg";
+    }
 }
