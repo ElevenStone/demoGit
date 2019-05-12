5206281a22397db4c393a4680c42f4e73b47d045
admin <sem00000@163.com>
2019-05-12 23:46:37 +0800

    Initial commit

---
+++ src/main/java/com/demo11/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.demo11.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/test1")
+    public String test1(){
+        return "123456789";
+    }
+}
